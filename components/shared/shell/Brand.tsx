75a0a208c127f9a7dcf5fa1a98ce22a774d1ddee
Bassel <bassel.alannan@gmail.com>
2025-09-18 20:33:37 +0400

    41

--- components/shared/shell/Brand.tsx
+++ components/shared/shell/Brand.tsx
@@ -1,7 +1,8 @@
-import app from '@/lib/app';
 import Image from 'next/image';
 import useTheme from 'hooks/useTheme';
 import { useRouter } from 'next/router';
+import logoDark from '@/public/logo-dark.png';
+import logoLight from '@/public/logo-light.png';
 
 interface BrandProps {
   size?: number; // size in pixels
@@ -14,17 +15,22 @@ const Brand = ({ size }: BrandProps) => {
   const finalSize = size !== undefined ? size : isDashboard ? 128 : 64;
 
   return (
-    
     <div className="flex pt-6 shrink-0 items-center">
       <div
         className="relative mx-auto"
         style={{ height: `${finalSize}px`, width: `${finalSize}px` }}
       >
         <Image
-          src={app.logoUrl}
-          alt={app.name}
+          src={logoDark}
+          alt="Logo"
           fill
-          className="object-contain"
+          className="object-contain block dark:hidden"
+        />
+        <Image
+          src={logoLight}
+          alt="Logo"
+          fill
+          className="object-contain hidden dark:block"
         />
       </div>
     </div>
