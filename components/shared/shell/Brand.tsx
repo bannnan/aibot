e31818fc44d455acdb4599e40b0230ad077533b7
Bassel <bassel.alannan@gmail.com>
2025-09-18 21:07:47 +0400

    41

--- components/shared/shell/Brand.tsx
+++ components/shared/shell/Brand.tsx
@@ -1,28 +1,22 @@
+import app from '@/lib/app';
 import Image from 'next/image';
 import useTheme from 'hooks/useTheme';
-import { useRouter } from 'next/router';
-import logoDark from '/logo-dark.png';
-import logoLight from '/logo-light.png';
 
 interface BrandProps {
   size?: number; // size in pixels
 }
 
-const Brand = ({ size }: BrandProps) => {
+const Brand = ({ size = 48 }: BrandProps) => {
   const { theme } = useTheme();
-  const router = useRouter();
-  const isDashboard = router.pathname.startsWith('/dashboard');
-  const finalSize = size !== undefined ? size : isDashboard ? 128 : 64;
-
   return (
     <div className="flex pt-6 shrink-0 items-center">
       <div
         className="relative mx-auto"
-        style={{ height: `${finalSize}px`, width: `${finalSize}px` }}
+        style={{ height: `${size}px`, width: `${size}px` }}
       >
         <Image
-          src={theme === 'dark' ? logoDark : logoLight}
-          alt="Logo"
+          src={app.logoUrl}
+          alt={app.name}
           fill
           className="object-contain"
         />
--- lib/app.ts
+++ lib/app.ts
@@ -4,6 +4,7 @@ import env from './env';
 const app = {
   version: packageInfo.version,
   name: 'Termynal',
+  logoUrl: '/logo.png',
   url: env.appUrl,
 };
 
